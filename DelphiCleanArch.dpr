e47abf96b95fa58dfab03bb7805b9f455d5d8313
Cesar Sanchez <tuky@Cesars-MacBook-Pro.local>
2025-09-05 21:31:10 -0300

    Fix logical folders and Add RegisterUser UseCases

--- DelphiCleanArch.dpr
+++ DelphiCleanArch.dpr
@@ -6,16 +6,14 @@ program DelphiCleanArch;
 
 uses
   System.SysUtils,
-  // --- Core Units ---
-  Core.Entities.User in 'Core\Entities\User.pas',
-  Core.Exceptions in 'Core\Exceptions\Exceptions.pas',
-  Core.Repositories.Interfaces in 'Core\Repositories\Interfaces.pas',
-  Core.Services.Interfaces in 'Core\Services\Interfaces.pas',
-  Core.UseCases.RegisterUser.Abstractions in 'Core\UseCases\RegisterUser\Abstractions.pas',
-  Core.UseCases.RegisterUser.Implementation in 'Core\UseCases\RegisterUser\Implementation.pas',
-  // --- Infrastructure Units ---
-  Infrastructure.Services.PasswordHasher in 'Infrastructure\Services\PasswordHasher.pas',
-  Infrastructure.Persistence.Firebird.UserRepository in 'Infrastructure\Persistence\Firebird\UserRepository.pas';
+  Core.Entities.User in 'Core\Core.Entities.User.pas',
+  Core.Exceptions in 'Core\Core.Exceptions.pas',
+  Core.Repositories.Interfaces in 'Core\Core.Repositories.Interfaces.pas',
+  Core.Services.Interfaces in 'Core\Core.Services.Interfaces.pas',
+  Core.UseCases.RegisterUser.Abstractions in 'Core\Core.UseCases.RegisterUser.Abstractions.pas',
+  Core.UseCases.RegisterUser.Implementation in 'Core\Core.UseCases.RegisterUser.Implementation.pas',
+  Infrastructure.Services.PasswordHasher in 'Infrastructure\Infrastructure.Services.PasswordHasher.pas',
+  Infrastructure.Persistence.Firebird.UserRepository in 'Infrastructure\Infrastructure.Persistence.Firebird.UserRepository.pas';
 
 begin
   try
